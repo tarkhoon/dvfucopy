8db1a42f8ea672ecd28f8a791046f7e0989c16d4
nikita ulyanov <nikita.ulyanov.2002@inbox.ru>
2021-10-20 23:27:55 +0900

    123

--- js/main.js
+++ js/main.js
@@ -1,37 +1,37 @@
-(function(){
-    var nav = document.querySelector('nav');
-    let timeout = null;
-    window.addEventListener('scroll', function(e){
-      if(!timeout){
-        timeout = setTimeout(function(){
-          var curScroll = document.documentElement.scrollTop;
+(function(){ // Функция для закрепления верхнего меню при прокрутке
+    var nav = document.querySelector('nav'); //Получение строки нав
+    let timeout = null; //Установление таймера
+    window.addEventListener('scroll', function(e){ //Проверка на прокрутку
+      if(!timeout){ //Закрепление меню, зацикливание
+        timeout = setTimeout(function(){ //Тело цикла
+          var curScroll = document.documentElement.scrollTop; //Получение информации о текущей прокрутке
           if(curScroll > 121){
-            nav.classList.add('fixed');
+            nav.classList.add('fixed'); //Закрепление
             previousScroll = curScroll;
-          }else if(curScroll < 121){
-            nav.classList.remove('fixed');
+          }else if(curScroll < 121){ 
+            nav.classList.remove('fixed'); // Открепление
             previousScroll = curScroll;
           }
-          clearTimeout(timeout);
+          clearTimeout(timeout); //Очистка таймера
           timeout = null;
         }, 0);
       }
     }); 
   }())
-
-let hovermenu = document.getElementById('callmenu');
-    hovermenu.onmouseover = over;
-    hovermenu.onmouseout = hide;
-var display = document.getElementById('menu1');
+//Выпадающее меню 1
+let hovermenu = document.getElementById('callmenu'); //Получение выпадающего меню 1
+    hovermenu.onmouseover = over; //Вызов функции over при наведении
+    hovermenu.onmouseout = hide; //Вызов функции hide при наведении
+var display = document.getElementById('menu1'); //получение элемента menu1
 
 function over(){
-    display.classList.add('show');
+    display.classList.add('show'); //Добавление класса show к элементу (показывает меню)
 }
 
 function hide(){
-    display.classList.remove('show');
+    display.classList.remove('show');//Удаление класса show к элементу (скрывает меню)
 }
-
+//Выпадающее меню 2
 let hovermenu2 = document.getElementById('callmenu2');
     hovermenu2.onmouseover = over2;
     hovermenu2.onmouseout = hide2;
@@ -44,10 +44,11 @@ function over2(){
 function hide2(){
     display2.classList.remove('show');
 }
-(function(){
-    setInterval(function() {
-        var hpic = document.getElementById('img1').offsetHeight;
-        document.getElementById('fadein').style.height = hpic + 'px';
+
+(function(){ //Выравнивание высоты блока с меняющимися картинками под высоту картинок для адаптивности при уменьшении
+    setInterval(function() { //Зацикливание
+        var hpic = document.getElementById('img1').offsetHeight; //Получение высоты картинок
+        document.getElementById('fadein').style.height = hpic + 'px'; //Выравнивание высоты блока под высоту картинок
       }, 0);
 }())
 
